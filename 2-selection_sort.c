15ee28aa351829dccde0f149bfe011de1c33a096
Malik <malikhussein229@gmail.com>
2024-02-20 04:25:12 -0800

    Creating files

---
+++ 2-selection_sort.c
@@ -0,0 +1,29 @@
+#include "sort.h"
+
+/**
+ * selection_sort - sorts an array of integers in
+ * ascending order using the Selection sort algorithm
+ * @array: pointer to the array
+ * @size: size of the array
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, min_val;
+	int temp;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min_val = i;
+		for (j = i + 1; j < size; j++)
+			if (array[j] < array[min_val])
+				min_val = j;
+
+		if (array[i] > array[min_val])
+		{
+			temp = array[min_val];
+			array[min_val] = array[i];
+			array[i] = temp;
+			print_array(array, size);
+		}
+	}
+}
